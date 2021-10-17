e3a4b51b887e60c9f532acdf9d7afd6d18828d8d
5erg1kal <santamaria.fajardo@gmail.com>
2021-10-17 13:42:32 +0200

    some improvements

--- functions/dao/context.js
+++ functions/dao/context.js
@@ -3,8 +3,6 @@ const NO_USER = 'nouser';
 function getUserId({ body = {} }) {
   const payload = body.originalDetectIntentRequest && body.originalDetectIntentRequest.payload;
   const sesId = body.session && body.session.replace(/.*\//, '');
-  console.trace(`body -> ${JSON.stringify(body)}`);
-  console.trace(`getUserId -> Request originalDetectIntentRequest: ${JSON.stringify(body.originalDetectIntentRequest)}`);
 
   const userMail = payload &&
     payload.data &&
@@ -20,10 +18,5 @@ function getUserId({ body = {} }) {
     payload.data.event &&
     payload.data.event.user || sesId || NO_USER;
 }
-
-/*function getValidDomain() {
-    const userMail = JSON.stringify(request.body.originalDetectIntentRequest.payload.data.data.personEmail);
-    return userMail.split('@')[1].split('.')[0];
-}*/
 
 module.exports = { getUserId };
--- web/web-main.js
+++ web/web-main.js
@@ -1,10 +1,40 @@
 const storage = window.localStorage;
-let textarea, responses, splashScreen, continueButton, warningMessage, currentPlace, currentSound, outerAudio;
+let textarea, responses, splashScreen, continueButton, warningMessage, currentPlace, currentTrack, outerAudio, audioController;
+let audioActive = false;
 
 const playList = {
-  biblioteca: {
-    soundUrl: 'localhost/musicaINteriores.mp3'
-  }
+  biblioteca: 'sounds/interiorCut2.mp3',
+  habitacion: 'sounds/interiorCut2.mp3',
+  zaguan: 'sounds/interiorCut2.mp3',
+  cocina: 'sounds/interiorCut2.mp3',
+  despensa: 'sounds/interiorCut2.mp3',
+  comedor: 'sounds/interiorCut2.mp3',
+  portalon: 'sounds/interiorCut2.mp3',
+  argamasilla: 'sounds/nightShadecut.mp3',
+  arco: 'sounds/nightShadecut.mp3',
+  arboleda: 'sounds/nightShadecut.mp3',
+  manzano: 'sounds/nightShadecut.mp3',
+  pinar: 'sounds/nightShadecut.mp3',
+  muro: 'sounds/nightShadecut.mp3',
+  callejon: 'sounds/nightShadecut.mp3',
+  posada: 'sounds/interiorCut2.mp3',
+  recepcion: 'sounds/interiorCut2.mp3',
+  escalera: 'sounds/interiorCut2.mp3',
+  rellano: 'sounds/interiorCut2.mp3',
+  bodega: 'sounds/interiorCut2.mp3',
+  dormitorio: 'sounds/interiorCut2.mp3',
+  calles: 'sounds/nightShadecut.mp3',
+  campos: 'sounds/nightShadecut.mp3',
+  acantilado: 'sounds/nightShadecut.mp3',
+  bosque: 'sounds/nightShadecut.mp3',
+  venta: 'sounds/nightShadecut.mp3',
+  recibidor: 'sounds/interiorCut2.mp3',
+  salon: 'sounds/interiorCut2.mp3',
+  distribuidor: 'sounds/interiorCut2.mp3',
+  alcoba: 'sounds/interiorCut2.mp3',
+  patio: 'sounds/interiorCut2.mp3',
+  prados: 'sounds/nightShadecut.mp3',
+  cancela: 'sounds/nightShadecut.mp3'
 };
 
 function documentReady() {
@@ -13,10 +43,14 @@ function documentReady() {
   splashScreen = document.querySelector('#splashScreen');
   warningMessage = document.querySelector('#warningMessage');
   continueButton = document.querySelector('#continueButton');
-  outerAudio = document.querySelector('#outerAudio').play();
+  audioController = document.querySelector('#audioController');
+  outerAudio = document.querySelector('#outerAudio');
+  currentPlace = storage.getItem("currentPlace");
+  document.addEventListener('click', setFocus);
   if(!getUID()) {
     continueButton.disabled = true;
   }
+  audioController.addEventListener("click", onOffAudio);
 }
 
 async function sendText({keyCode, currentTarget}) {
@@ -49,6 +83,9 @@ function restartGame() {
   if(!getUID()) {
     startGame();
     createUID();
+    setTimeout(() => {
+      responses.append(quixoteChat("Hola?, hay alguien ahí?"));
+    }, 2000);
   } else {
     toogleElementOpacity(warningMessage, false);
   }
@@ -61,10 +98,10 @@ function continueGame() {
 
 function startGame() {
   toogleElementOpacity(splashScreen, true);
-  document.querySelector('#enterText').addEventListener('click', setFocus);
   textarea.addEventListener("keyup", sendText);
   setInputWidth();
   setFocus();
+  if(audioActive) outerAudio.play(playList[currentPlace]);
 }
 
 function showLoading(target) {
@@ -104,17 +141,41 @@ async function request(input) {
   // set bold
   text = text.replace(/\*([^*]+?)\*/g, '<b>$1</b>');
   currentPlace = ((text.match(/src="([^&]*)"/) || 'none')[1].match(/([^/]+?).$/) || 'none')[0].replace(/.png/,'');
-  if( currentPlace ) {
-    playMusic(currentPlace);
+  if( currentPlace !== "n" ) {
+    storage.setItem("currentPlace", currentPlace);
+    if(audioActive && playList[currentPlace] && currentTrack !== playList[currentPlace]) {
+      currentTrack = playList[currentPlace];
+      playMusic(currentTrack);
+    }
   }
   saveLastResponse(text);
   return text;
 }
 
-function playMusic(place) {
-  currentSound = playList[place];
-  console.log(currentSound);
+function playMusic(soundTrack) {
+  outerAudio.src = soundTrack || "sounds/nightShadecut.mp3";
+  outerAudio.volume = 0.1;
+  outerAudio.play();
 }
+
+function pauseMusic() {
+  currentTrack = undefined;
+  outerAudio.pause();
+}
+
+function onOffAudio() {
+  if (!audioActive) {
+    audioController.children[0].src = "images/sound-icon.png";
+    audioController.children[1].innerHTML = "MUSIC ON";
+    playMusic(playList[currentPlace]);
+    audioActive = true;
+  } else {
+    audioController.children[0].src = "images/mute-icon.png";
+    audioController.children[1].innerHTML = "MUSIC OFF";
+    pauseMusic();
+    audioActive = false;
+  }
+};
 
 function saveLastResponse(text) {
   storage.setItem('last', text);
