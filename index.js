ae4d627dc3e2a05d19d55cde98fcb6578a6fafb6
5erg1kal <santamaria.fajardo@gmail.com>
2026-03-16 11:32:29 +0100

    remove ssl

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4500;
-
-const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'credentials/key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'credentials/cert.pem')),
-};
 
 const {WebhookClient} = require('dialogflow-fulfillment');
 
