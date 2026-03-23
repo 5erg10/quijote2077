f7844fd0bb285afb42c05ff95fb5cc6e8a2c81f8
5erg1kal <santamaria.fajardo@gmail.com>
2026-03-23 09:33:56 +0100

    remove unecesary files

--- functions/intents/welcome.js
+++
@@ -1,8 +0,0 @@
-function welcomeResponse() {
-    return agent => {      
-      agent.add('Hola aventurero!, no sé si eres un valiente o un inconsciente al saludarme, pero en fin, ya lo descubriremos si estas dispuesto a embarcarte en esta aventura. ¿Quieres comenzar la gesta para convertirte en un ingenioso hidalgo?');
-      return Promise.resolve();
-    };
-}
-
-module.exports = { welcomeResponse };
